12a34ecc3fd392e8113cf990276118f8ceb26ba1
ThomasFaria <thomas.faria@ensae.fr>
2024-09-09 16:14:31 +0000

    optimize

--- create-mapping.py
+++ create-mapping.py
@@ -2,6 +2,7 @@ import pandas as pd
 import unicodedata
 
 table_corres = pd.read_excel("table-correspondance-naf2025.xls", dtype=str)
+notes_ex = pd.read_excel("notes-explicatives-naf2025.xlsx", dtype=str)
 
 
 table_corres = (
@@ -20,19 +21,18 @@ table_corres = (
 )
 
 naf08 = list(pd.unique(table_corres["naf08_niv5"]))
-mapping = {}
-for code08 in naf08:
-    subset = table_corres.loc[table_corres["naf08_niv5"] == code08]
-    mapping[code08] = {}
-    mapping[code08]["libelle"] = pd.unique(subset["lib_naf08_niv5"])[0]
-    mapping[code08]["naf25"] = {
-        row["naf25_niv5"]: {"libelle": row["lib_naf25_niv5"]} for _, row in subset.iterrows()
+
+mapping = {
+    code08: {
+        "libelle": pd.unique(subset["lib_naf08_niv5"])[0],
+        "naf25": {
+            row["naf25_niv5"]: {"libelle": row["lib_naf25_niv5"]} for _, row in subset.iterrows()
+        },
     }
+    for code08, subset in table_corres.groupby("naf08_niv5")
+}
 
-notes_ex = pd.read_excel("notes-explicatives-naf2025.xlsx", dtype=str)
 notes_ex["Code.NACE.Rev.2.1"] = notes_ex["Code.NACE.Rev.2.1"].str.replace(".", "", regex=False)
-
-
 notes_ex = notes_ex.iloc[:, [6, 9, 10, 11, 12, 13]].rename(
     columns={
         "Code.NACE.Rev.2.1": "naf08_niv4",
@@ -42,44 +42,34 @@ notes_ex = notes_ex.iloc[:, [6, 9, 10, 11, 12, 13]].rename(
 )
 
 
+def normalize_value(value):
+    return unicodedata.normalize("NFKC", value) if pd.notna(value) else None
+
+
 for code08 in mapping.keys():
     for code25 in mapping[code08]["naf25"].keys():
-        row = notes_ex[notes_ex["naf08_niv4"] == code25]
+        row = notes_ex.loc[notes_ex["naf08_niv4"] == code25]
+
         if row.empty:
-            if notes_ex[notes_ex["naf08_niv4"] == code25[:-1]].shape[0] == 1:
-                row = notes_ex[notes_ex["naf08_niv4"] == code25[:-1]]
-            else:
+            row = notes_ex.loc[notes_ex["naf08_niv4"] == code25[:-1]]
+            if row.shape[0] != 1:
                 print(f"PROBLEM WITH {code25}")
                 continue
 
-        notes = (
-            unicodedata.normalize("NFKC", row.notes.iloc[0])
-            if pd.notna(row.notes.iloc[0])
-            else None
-        )
-        comprend = (
-            unicodedata.normalize("NFKC", row.Comprend.iloc[0])
-            if pd.notna(row.Comprend.iloc[0])
-            else None
-        )
-        comprend_aussi = (
-            unicodedata.normalize("NFKC", row["Comprend.aussi"].iloc[0])
-            if pd.notna(row["Comprend.aussi"].iloc[0])
-            else None
-        )
+        notes = normalize_value(row.notes.iloc[0])
+        comprend = normalize_value(row.Comprend.iloc[0])
+        comprend_aussi = normalize_value(row["Comprend.aussi"].iloc[0])
+        comprend_pas = normalize_value(row["Ne.comprend.pas"].iloc[0])
+
         comprend_all = (
             "\n".join(filter(None, [comprend, comprend_aussi]))
             if any([comprend, comprend_aussi])
             else None
         )
-        comprend_pas = (
-            unicodedata.normalize("NFKC", row["Ne.comprend.pas"].iloc[0])
-            if pd.notna(row["Ne.comprend.pas"].iloc[0])
-            else None
-        )
+
         notes_expli = {
             "notes": notes,
             "comprend": comprend_all,
             "comprend_pas": comprend_pas,
         }
-        mapping[code08]["naf25"][code25] = mapping[code08]["naf25"][code25] | notes_expli
+        mapping[code08]["naf25"][code25].update(notes_expli)
