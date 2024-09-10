a07c79d4be8d86484a1fd33869cf867a67bd2fe4
ThomasFaria <thomas.faria@ensae.fr>
2024-09-10 11:51:11 +0000

    make the join for univoque

--- create-mapping.py
+++ create-mapping.py
@@ -1,9 +1,17 @@
 import pandas as pd
 import unicodedata
+import duckdb
+
+
+def normalize_value(value):
+    return unicodedata.normalize("NFKC", value) if pd.notna(value) else None
+
 
 table_corres = pd.read_excel("table-correspondance-naf2025.xls", dtype=str)
 notes_ex = pd.read_excel("notes-explicatives-naf2025.xlsx", dtype=str)
 
+URL = "s3://projet-ape/extractions/20240812_sirene4.parquet"
+URL_OUT = "s3://projet-ape/extractions/20240812_sirene4_univoques.parquet"
 
 table_corres = (
     table_corres.iloc[:, [1, 3, 2, 10, 5, 11]]
@@ -19,8 +27,6 @@ table_corres = (
     )
     .apply(lambda col: col.str.replace(".", "", regex=False) if col.dtype == "object" else col)
 )
-
-naf08 = list(pd.unique(table_corres["naf08_niv5"]))
 
 mapping = {
     code08: {
@@ -40,11 +46,6 @@ notes_ex = notes_ex.iloc[:, [6, 9, 10, 11, 12, 13, 7, 8]].rename(
         "Note.générale": "notes",
     }
 )
-
-
-def normalize_value(value):
-    return unicodedata.normalize("NFKC", value) if pd.notna(value) else None
-
 
 for code08 in mapping.keys():
     for code25 in mapping[code08]["naf25"].keys():
@@ -76,3 +77,39 @@ for code08 in mapping.keys():
             "comprend_pas": comprend_pas,
         }
         mapping[code08]["naf25"][code25].update(notes_expli)
+
+
+univoques = [naf08 for naf08 in mapping.keys() if len(mapping[naf08]["naf25"]) == 1]
+multivoques = [naf08 for naf08 in mapping.keys() if len(mapping[naf08]["naf25"]) > 1]
+
+con = duckdb.connect(database=":memory:")
+
+# Construct the CASE statement from the dictionary mapping
+case_statement = "CASE "
+for key, value in mapping.items():
+    case_statement += f"WHEN code_naf08 = '{key}' THEN '{[*value["naf25"]][0]}' "
+case_statement += "ELSE NULL END AS code_naf25"
+
+# SQL query with renamed column and new column using CASE for mapping
+query = f"""
+    SELECT
+        *,
+        apet_finale AS code_naf08,
+        {case_statement}
+    FROM
+        read_parquet('{URL}')
+    WHERE
+        code_naf08 IN ('{"', '".join(univoques)}')
+"""
+
+con.execute(
+    f"""
+    COPY
+    ({query})
+    TO '{URL_OUT}'
+    (FORMAT 'parquet')
+;
+"""
+)
+
+con.close()
