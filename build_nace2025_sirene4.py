1b2dcca29bbb94bf9f541601f8e8f7b5f9292540
ThomasFaria <thomas.faria@ensae.fr>
2024-10-30 16:36:34 +0000

    add script to rebuild entire database

---
+++ build_nace2025_sirene4.py
@@ -0,0 +1,90 @@
+import pandas as pd
+
+from src.constants.paths import (
+    URL_GROUND_TRUTH,
+    URL_SIRENE4_EXTRACTION,
+    URL_SIRENE4_MULTIVOCAL_FINAL,
+    URL_SIRENE4_NACE2025,
+    URL_SIRENE4_UNIVOCAL,
+)
+from src.utils.data import get_file_system
+
+fs = get_file_system()
+
+VAR_TO_KEEP = [
+    "liasse_numero",
+    "libelle",
+    "evenement_type",
+    "cj",
+    "activ_nat_et",
+    "liasse_type",
+    "activ_surf_et",
+    "activ_sec_agri_et",
+    "activ_nat_lib_et",
+    "activ_perm_et",
+]
+
+# Pour les univoques, on enleve les doublons de liasse seulement
+data_univocal = pd.read_parquet(URL_SIRENE4_UNIVOCAL, filesystem=fs)
+data_univocal = data_univocal[~data_univocal.duplicated(subset="liasse_numero")]
+
+# Pour les multivocaux issue de l'annotation humaine on enleve les doublons de liasse et on renomme la colonne apet_manual en nace2025
+data_multivocal_ground_truth = (
+    pd.read_parquet(URL_GROUND_TRUTH, filesystem=fs)
+    .rename(columns={"apet_manual": "nace2025"})
+    .loc[:, ["liasse_numero", "nace2025"]]
+)
+data_multivocal_ground_truth = data_multivocal_ground_truth.loc[
+    ~data_multivocal_ground_truth.duplicated(subset="liasse_numero")
+]
+
+# Pour les multivocaux du LLM on enlève les données qui sont dans l'annotation humaine
+data_multivocal = pd.read_parquet(URL_SIRENE4_MULTIVOCAL_FINAL, filesystem=fs)
+data_multivocal = data_multivocal.loc[
+    ~data_multivocal["liasse_numero"].isin(data_multivocal_ground_truth["liasse_numero"].tolist())
+]
+
+# Pour les données de sirene 4, on enlève les doublons de liasse
+data_sirene4 = pd.read_parquet(URL_SIRENE4_EXTRACTION, filesystem=fs).loc[:, VAR_TO_KEEP]
+data_sirene4 = data_sirene4[~data_sirene4.duplicated(subset="liasse_numero")]
+
+# On rajoute les variables annexes aux multivoques, univoques et ground truth contenant l'annotation nace2025
+data_univocal = data_univocal.merge(data_sirene4, on="liasse_numero", how="left")
+data_multivocal = data_multivocal.merge(data_sirene4, on="liasse_numero", how="left")
+data_multivocal_ground_truth = data_multivocal_ground_truth.merge(
+    data_sirene4, on="liasse_numero", how="left"
+)
+
+# few lines are still duplicated, remove them before merge. Old Label Studio pipeline was not 100% perfect
+data_multivocal_ground_truth = data_multivocal_ground_truth.drop_duplicates(
+    subset=[v for v in VAR_TO_KEEP if v != "liasse_numero"]
+)
+
+# On reconstruit les données multivoques en réinjectant les code nace 2025 pour les doublons
+data_sirene4_multivoque = data_sirene4.loc[
+    ~data_sirene4["liasse_numero"].isin(data_univocal["liasse_numero"].tolist()), VAR_TO_KEEP
+]
+data_multivocal_resampled = (
+    data_multivocal.merge(
+        data_sirene4_multivoque, on=[v for v in VAR_TO_KEEP if v != "liasse_numero"], how="left"
+    )
+    .rename(columns={"liasse_numero_y": "liasse_numero"})
+    .drop(columns=["liasse_numero_x"])
+)
+data_multivocal_ground_truth_resampled = (
+    data_multivocal_ground_truth.merge(
+        data_sirene4_multivoque, on=[v for v in VAR_TO_KEEP if v != "liasse_numero"], how="left"
+    )
+    .rename(columns={"liasse_numero_y": "liasse_numero"})
+    .drop(columns=["liasse_numero_x"])
+)
+# Certains univoques sont les dans les multivoques, on les enlève
+data_multivocal_ground_truth_resampled.dropna(subset=["liasse_numero"], inplace=True)
+
+data_sirene4_nace2025 = pd.concat(
+    [data_univocal, data_multivocal_resampled, data_multivocal_ground_truth_resampled], axis=0
+)
+
+assert data_sirene4_nace2025.duplicated(subset="liasse_numero").sum() == 0
+
+data_sirene4_nace2025.to_parquet(URL_SIRENE4_NACE2025, filesystem=fs)
