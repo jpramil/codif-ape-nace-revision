aa10210cc24c4ebec659e34256a4c1434df5491f
ThomasFaria <thomas.faria@ensae.fr>
2025-03-06 15:54:27 +0000

    [fix] trying to fix path issue

--- encode-ambiguous-rag.py
+++ encode-ambiguous-rag.py
@@ -69,7 +69,8 @@ def encode_ambiguous(
     batch_prompts = [p.prompt for p in prompts]
 
     # Initialize LLM
-    local_path_model = os.path.expanduser(f"~/.cache/huggingface/hub/{llm_name}")
+    local_path_model = os.path.expanduser(f"{os.getenv("LOCAL_PATH")}/{llm_name}")
+    print(os.listdir(local_path_model))
     llm = LLM(model=local_path_model, **MODEL_TO_ARGS.get(llm_name, {}))
     tokenizer = llm.get_tokenizer()
 
