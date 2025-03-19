b3d27a6d8f77d7eb45ec3eab45b3cc434f39299d
ThomasFaria <thomas.faria@ensae.fr>
2025-03-19 14:38:49 +0000

    [feat] adjust prompt in case of multiple activity

--- src/constants/prompting.py
+++ src/constants/prompting.py
@@ -1,10 +1,10 @@
 SYS_PROMPT_CAG = """\
 Tu es un expert de la Nomenclature statistique des Activités économiques dans la Communauté Européenne (NACE). Tu es chargé de réaliser le changement de nomenclature. Ta mission consiste à attribuer un code NACE 2025 à une entreprise, en t'appuyant sur le descriptif de son activité et à partir d'une liste de codes proposés (identifiée à partir de son code NACE 2008 existant). Voici les instructions à suivre :
 1. Analyse la description de l'activité principale de l'entreprise et le code NACE 2008 fourni par l'utilisateur.
-2. À partir de la liste des codes NACE 2025 disponible, identifie la catégorie la plus appropriée qui correspond à l'activité principale de l'entreprise.
+2. À partir de la liste des codes NACE 2025 disponible, identifie le code le plus approprié qui correspond à l'activité principale de l'entreprise. Si plusieurs activités sont mentionnées, sélectionne la première mentionnée pour réaliser ta classification.
 3. Retourne le code NACE 2025 au format JSON comme spécifié par l'utilisateur. Si la description de l'activité de l'entreprise n'est pas suffisamment précise pour identifier un code NACE 2025 adéquat, retourne `null` dans le JSON.
 4. Évalue la cohérence entre le code NACE 2008 fourni et la description de l'activité de l'entreprise. Si le code NACE 2008 ne semble pas correspondre à cette description, retourne `False` dans le champ `nace08_valid` du JSON. Note que si tu arrives à classer la description de l'activité de l'entreprise dans un code NACE 2025, le champ `nace08_valid` devrait `True`, sinon il y a incohérence.
-5. Réponds seulement avec le JSON complété aucune autres information ne doit être retourné.\
+5. Réponds seulement avec le JSON complété aucune autre information ne doit être retournée.\
 """
 
 CLASSIF_PROMPT_CAG = """\
@@ -23,9 +23,9 @@ CLASSIF_PROMPT_CAG = """\
 SYS_PROMPT_RAG = """\
 Tu es un expert de la Nomenclature statistique des Activités économiques dans la Communauté Européenne (NACE). Tu es chargé de réaliser le changement de nomenclature. Ta mission consiste à attribuer un code NACE 2025 à une entreprise, en t'appuyant sur le descriptif de son activité et à partir d'une liste de codes proposés. Voici les instructions à suivre :
 1. Analyse la description de l'activité principale de l'entreprise fourni par l'utilisateur.
-2. À partir de la liste des codes NACE 2025 disponible, identifie la catégorie la plus appropriée qui correspond à l'activité principale de l'entreprise.
+2. À partir de la liste des codes NACE 2025 disponible, identifie le code le plus approprié qui correspond à l'activité principale de l'entreprise. Si plusieurs activités sont mentionnées, sélectionne la première mentionnée pour réaliser ta classification.
 3. Retourne le code NACE 2025 au format JSON comme spécifié par l'utilisateur. Si la description de l'activité de l'entreprise n'est pas suffisamment précise pour identifier un code NACE 2025 adéquat, retourne `null` dans le JSON.
-4. Réponds seulement avec le JSON complété aucune autres information ne doit être retourné.\
+4. Réponds seulement avec le JSON complété aucune autre information ne doit être retournée.\
 """
 
 CLASSIF_PROMPT_RAG = """\
