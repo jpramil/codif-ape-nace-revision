9a8613a4344d3cdd7e9bfe5a5f04af7a405aa7e0
ThomasFaria <thomas.faria@ensae.fr>
2025-07-10 10:39:31 +0000

    [refactor] improve main file for ambiguous

--- src/encode_ambiguous.py
+++ src/encode_ambiguous.py
@@ -5,7 +5,6 @@ import os
 import mlflow
 
 import config
-from constants.llm import LLM_MODEL, MODEL_TO_ARGS
 from constants.paths import URL_SIRENE4_EXTRACTION
 from evaluation.evaluator import Evaluator
 from strategies.cag import CAGStrategy
@@ -14,25 +13,14 @@ from utils.data import get_ambiguous_data
 
 config.setup()
 
-# third = 1
-# llm_name = "Qwen/Qwen2.5-0.5B"
-# STRATEGY_MAP = {
-#     "rag": RAGStrategy,
-#     "cag": CAGStrategy,
-# }
-# strategy_cls = STRATEGY_MAP["rag"]
-
 
 async def run_encode(strategy_cls, experiment_name, run_name, llm_name, third):
     strategy = strategy_cls(
         generation_model=llm_name,
     )
+    data = get_ambiguous_data(strategy.mapping, third, only_annotated=False)
 
-    data = get_ambiguous_data(strategy.mapping, third, only_annotated=True)
-
-    data = data.head(20)
-    # prompts = await strategy.get_prompts(data)
-    prompts = asyncio.run(strategy.get_prompts(data))
+    prompts = await strategy.get_prompts(data)
 
     mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
     mlflow.set_experiment(experiment_name)
@@ -69,7 +57,7 @@ if __name__ == "__main__":
     parser.add_argument("--strategy", choices=["rag", "cag"], required=True)
     parser.add_argument("--experiment_name", type=str, default="Test")
     parser.add_argument("--run_name", type=str, default=None)
-    parser.add_argument("--llm_name", type=str, default=LLM_MODEL, choices=MODEL_TO_ARGS.keys())
+    parser.add_argument("--llm_name", type=str, default="Qwen/Qwen2.5-0.5B")
     parser.add_argument("--third", type=int, default=None)
 
     args = parser.parse_args()
